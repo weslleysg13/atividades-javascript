8a70edbfccfb82ae26aabf8b47b8e4ac14cbcfda
Weslley SG <42002498+weslleysg13@users.noreply.github.com>
2025-08-26 22:22:21 -0300

    Função maiorNumero(listadeNumeros) adicionada e detalhada

--- atividade-funcoes.js
+++ atividade-funcoes.js
@@ -26,7 +26,21 @@ function fatorial(numero) {
   return console.log("O fatorial do número", chamadadaFuncao, "é:", resultado);
 }
 
+function maiorNumero(listadeNumeros){
+    let maior = listadeNumeros[0]; // Variável maior sendo inicializada com o primeiro número do array
+    for (posicao = 1; posicao < listadeNumeros.length; posicao++){ // For inicializado na posicao 1 pois o número da posição 0 já está armazenado como maior
+        if (listadeNumeros[posicao] > maior) { // se o número na posição atual do array for maior do que o anterio registrado, ele se torna o maior
+            maior = listadeNumeros[posicao];
+        }
+    }
+
+    return console.log("O maior número do array é o", maior);
+}
+
 parImpar(6);
 parImpar(3);
 
 fatorial(5);
+
+const array = [1,3,2,5,8];
+maiorNumero(array);
